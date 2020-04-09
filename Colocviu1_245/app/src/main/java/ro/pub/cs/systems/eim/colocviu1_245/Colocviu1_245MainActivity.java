629934432af226f8d25b088ad1b08601e5757996
zubair farahi <zubair.farahi121@yahoo.com>
2020-04-09 12:55:19 +0300

    rezolvareA

---
+++ Colocviu1_245/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_245/Colocviu1_245MainActivity.java
@@ -0,0 +1,16 @@
+package ro.pub.cs.systems.eim.colocviu1_245;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colocviu1_245MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colocviu1_245_main);
+    }
+
+
+}
